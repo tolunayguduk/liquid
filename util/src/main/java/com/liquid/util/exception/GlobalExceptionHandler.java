03b60ae21c045c05af4ef0680c13c9c29065bf96
Tolunay Güdük <guduktolunay@gmail.com>
2023-03-25 01:30:10 +0300

    error management

---
+++ util/src/main/java/com/liquid/util/exception/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.liquid.util.exception;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+public abstract class GlobalExceptionHandler {
+
+	private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+	@ExceptionHandler(value = CustomException.class)
+	public ResponseEntity<?> exception(CustomException exception) {
+		Exception ex = Exception.getException(Long.valueOf(exception.getMessage()));
+		logger.info("(" + ex.getCode() + ") " + ex.getExplanation());
+		return new ResponseEntity<>(ex.getExplanation(), ex.getStatus());
+	}
+
+	@ExceptionHandler()
+	public ResponseEntity<?> exception(java.lang.Exception exception) {
+		logger.info(exception.getLocalizedMessage());
+		return new ResponseEntity<>(exception.getLocalizedMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+}
