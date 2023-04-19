e525cab1093e3092c45a6d3ab62876fb44402a02
Tolunay Güdük <guduktolunay@gmail.com>
2023-04-20 01:09:52 +0300

    mail

---
+++ notification/src/main/java/com/liquid/notification/util/MailSender.java
@@ -0,0 +1,29 @@
+package com.liquid.notification.util;
+
+import java.util.Properties;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+public class MailSender {
+
+	@Bean
+	public JavaMailSender send() {
+		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+		mailSender.setHost("smtp.gmail.com");
+		mailSender.setPort(587);
+
+		mailSender.setUsername("guduktolunay@gmail.com");
+		mailSender.setPassword("T.g5356032012");
+
+		Properties props = mailSender.getJavaMailProperties();
+		props.put("mail.transport.protocol", "smtp");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.debug", "true");
+
+		return mailSender;
+	}
+
+}
