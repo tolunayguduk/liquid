e6f79b159c57893aa3b7eaea5abe89173aa10e7f
Tolunay Guduk <guduktolunay@gmail.com>
2023-12-06 21:05:04 +0300

    ui

--- liquid-ui/src/App.js
+++ liquid-ui/src/App.js
@@ -1,22 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="http://localhost:8000/report/hello"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        LIQUID
       </header>
     </div>
   );
