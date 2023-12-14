2593b8ad42fc62403526354058b246392cc25f10
Tolunay Guduk <guduktolunay@gmail.com>
2023-12-14 23:28:06 +0300

    spring-security

--- gateway/src/main/java/com/liquid/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/liquid/gateway/GatewayApplication.java
@@ -2,7 +2,9 @@ package com.liquid.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 
 @SpringBootApplication
 @EnableEurekaClient
